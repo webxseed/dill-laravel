4cad8aeea17d56a2e6cd639b8f547e234497caf5
Wajdy Jeries <wajdy@email.com>
2026-03-11 01:39:03 +0200

    Fix CSRF issue for API routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -20,7 +20,9 @@ return Application::configure(basePath: dirname(__DIR__))
             'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
         ]);
 
-        //
+        $middleware->validateCsrfTokens(except: [
+            'api/*',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
