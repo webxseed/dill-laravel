e1080069f4c820605056aa4f9ba34f0cbf7ab1a7
thisiswajdy-code <thisiswajdy@gmail.com>
2026-03-24 13:57:36 +0200

    .

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,6 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>DIIL - Defects and Internal Interfaces Lab</title>
     <meta name="description" content="Defects and Internal Interfaces Lab - Tel Aviv University" />
+    <style>
+      input[type="file"] {
+            background: #be6e38;
+        color: #fff;
+        padding: 6px;
+        border-radius: 10px;
+      }
+    </style>
   </head>
   <body>
     <div id="root"></div>
